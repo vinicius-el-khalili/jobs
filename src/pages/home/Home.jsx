c6cff099c52314ca2eba1415bc76112b87714d6b
vinicius-el-khalili <velkhaliliborsato@gmail.com>
2023-06-11 10:46:13 -0300

    ...

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,65 +1,14 @@
 import style from "./Home.module.scss"
 import Card from "./card"
+import { data } from "../../../data/data"
 
 const Home = () => {
-    const data = [
-        {
-            title:"Glassdoor",
-            url:"https://www.glassdoor.com/",
-            img:"",
-        },
-        {
-            title:"Linkedin",
-            url:"https://www.linkedin.com/",
-            img:"",
-        },
-        {
-            title:"Catho",
-            url:"https://www.catho.com.br/",
-            img:"",
-        },
-        {
-            title:"Vagas",
-            url:"https://www.vagas.com.br/",
-            img:"",
-        },
-        {
-            title:"Gupy",
-            url:"https://www.gupy.io/",
-            img:"",
-        },
-        {
-            title:"Trampos",
-            url:"https://trampos.co/",
-            img:"",
-        },
-        {
-            title:"Trabalha Brasil",
-            url:"https://www.trabalhabrasil.com.br/",
-            img:"",
-        },
-        {
-            title:"BNE",
-            url:"https://www.bne.com.br/",
-            img:"",
-        },
-        {
-            title:"Jobs",
-            url:"https://solides.jobs/",
-            img:"",
-        },
-        {
-            title:"ProgramaThor",
-            url:"https://programathor.com.br/",
-            img:"",
-        },
-    
-    ]
+
     return(
         <div className={style.Home}>
             {data.map( platform => (
             
-            <Card title={platform.title} Furl={platform.url} key={platform.title}/>
+            <Card title={platform.title} url={platform.url} key={platform.title}/>
 
             ))}
         </div>
--- src/pages/home/card.jsx
+++ src/pages/home/card.jsx
@@ -1,11 +1,12 @@
 import style from "./Home.module.scss"
 import Link from "next/link"
 
-const Card = ({title,Furl}) => {
+const Card = ({title,url}) => {
     return(
         <Link href={"https://google.com"} className={style.link} target="_blank">
             <div className={style.Card}>
                 <h1>{title}</h1>
+                <h2>{url}</h2>
             </div>
         </Link>
     )
